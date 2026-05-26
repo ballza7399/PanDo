68a248a4e6e9eb6cea9005936ef3f0960189c9d4
Kittisak Sakmeang <kittisak@ditruz.com>
2026-05-27 01:25:58 +0700

    chore: ลบไฟล์ที่ไม่จำเป็นออกจากโปรเจกต์
    - PanDo.csproj.nuget.g.props: ลบไฟล์ที่ไม่จำเป็น
    - PanDo.csproj.nuget.g.targets: ลบไฟล์ที่ไม่จำเป็น
    - project.assets.json: ลบไฟล์ที่ไม่จำเป็น
    - project.nuget.cache: ลบไฟล์ที่ไม่จำเป็น

--- obj/Debug/net10.0/PanDo.AssemblyInfo.cs
+++ obj/Debug/net10.0/PanDo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PanDo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c340352ba5ac55ad61408db1ebbfd21c90e991d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8c9ec459c8269d4573498a4a8beee20f535148")]
 [assembly: System.Reflection.AssemblyProductAttribute("PanDo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PanDo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
